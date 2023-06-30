9b61e1bd00b33ad778def3a4a5c0d43f511f5420
ShayanJamil519 <shayanjamil500@gmail.com>
2023-06-30 20:56:16 +0500

    Files Setup Done ✅

---
+++ src/pages/Home/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from '../../components/Home/Navbar/Navbar'
+
+const Home = () => {
+  return (
+    <div className='home__container'>
+
+      <Navbar />
+    </div>
+  )
+}
+
+export default Home
